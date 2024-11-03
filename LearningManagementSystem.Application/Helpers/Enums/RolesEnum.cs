3dbc6a6286e40921f022c71a6d2089b69b671410
Nihadname <nihadcoding@gmail.com>
2024-11-03 15:51:47 +0400

    updated packages

---
+++ LearningManagementSystem.Application/Helpers/Enums/RolesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Application.Helpers.Enums
+{
+    public enum RolesEnum
+    {
+        Admin,
+    
+    }
+}
