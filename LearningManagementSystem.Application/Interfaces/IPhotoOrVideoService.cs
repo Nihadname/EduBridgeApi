534c26d4281c1b2b77e94ed92ece72646ad5418a
Nihadname <nihadcoding@gmail.com>
2024-12-29 01:40:58 +0400

    done

---
+++ LearningManagementSystem.Application/Interfaces/IPhotoOrVideoService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Application.Interfaces
+{
+    public interface IPhotoOrVideoService
+    {
+        Task<string> UploadPhotoAsync(IFormFile file);
+        Task DeletePhotoAsync(string imageUrl);
+    }
+}
